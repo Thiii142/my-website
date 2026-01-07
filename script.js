fa64ef13eb743adbbffead60811aa01b6ecaa75b
Thiii142 <thi@kanglongda.com.cn>
2026-01-07 14:06:44 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+function previewImages(event) {
+    const preview = document.getElementById("photoPreview");
+    preview.innerHTML = ""; // xoá ảnh cũ
+
+    const files = event.target.files;
+
+    for (let file of files) {
+        const reader = new FileReader();
+
+        reader.onload = function(e) {
+            const img = document.createElement("img");
+            img.src = e.target.result;
+            preview.appendChild(img);
+        };
+
+        reader.readAsDataURL(file);
+    }
+}
+function showTab(tabId) {
+    const tabs = document.querySelectorAll(".tab-content");
+    tabs.forEach(tab => tab.classList.remove("active"));
+
+    document.getElementById(tabId).classList.add("active");
+}
